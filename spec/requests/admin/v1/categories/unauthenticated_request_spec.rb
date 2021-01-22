6ebdfd14d7c62dc6a821994f1c25f3ed92e60a15
André de Souza Alves <alves.andre.ds@gmail.com>
2021-01-22 20:44:51 -0300

    Fix specs

--- spec/requests/admin/v1/categories/unauthenticated_request_spec.rb
+++ spec/requests/admin/v1/categories/unauthenticated_request_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "Admin::V1::Coupons as without authentication", type: :request do
+RSpec.describe "Admin::V1::Categories as without authentication", type: :request do
   context "GET /categories" do
     let(:url) {"/admin/v1/categories"}
     let!(:categories) {create_list(:category, 5)}
