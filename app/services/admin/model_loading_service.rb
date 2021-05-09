e9308a1b75f44faaef09ca6b544b11c18eccbaa5
André de Souza Alves <alves.andre.ds@gmail.com>
2021-05-08 23:07:48 -0300

    Correction of the Paging Service

--- app/services/admin/model_loading_service.rb
+++ app/services/admin/model_loading_service.rb
@@ -6,15 +6,15 @@ module Admin
       @searchable_model = searchable_model
       @params = params || {}
       @records = []
-      @pagination = { page: @params[:page].to_i, length: @params[:length].to_i }
+      @pagination = {}
     end
 
     def call
       set_pagination_values
       searched = search_records(@searchable_model)
-      @records = searched.order(@params[:order].to_h).paginate(@pagination[:page], @pagination[:length])
-      total_pages = (searched.count / @pagination[:length].to_f).ceil
-      @pagination.merge!(total: searched.count, total_pages: total_pages)
+      @records = searched.order(@params[:order].to_h).paginate(@params[:page], @params[:length])
+
+      set_pagination_attributes(searched.count)
     end
 
     private
@@ -28,9 +28,20 @@ module Admin
     end
 
     def set_pagination_values
-      @pagination[:page] = @searchable_model.model::DEFAULT_PAGE if @pagination[:page].zero?
-      @pagination[:length] = @searchable_model.model::MAX_PER_PAGE if @pagination[:length].zero?
+      @params[:page] = @params[:page].to_i
+      @params[:length] = @params[:length].to_i
+      @params[:page] = @searchable_model.model::DEFAULT_PAGE if @params[:page].zero?
+      @params[:length] = @searchable_model.model::MAX_PER_PAGE if @params[:length].zero?
     end
-    
+
+    def set_pagination_attributes(total_filtered)
+      total_pages = (total_filtered / @params[:length].to_f).ceil
+      @pagination.merge!(
+        page: @params[:page],
+        length: @records.count,
+        total: total_filtered,
+        total_pages: total_pages
+      )
+    end
   end
 end
