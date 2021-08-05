feb9d760bf1a057fd5a650fd0a240600b772be74
André de Souza Alves <alves.andre.ds@gmail.com>
2021-08-05 00:40:33 -0300

    Model for WishList

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,10 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   include DeviseTokenAuth::Concerns::User
+
+  has_many :wish_items
 
   validates :name, presence: true
   validates :profile, presence: true
