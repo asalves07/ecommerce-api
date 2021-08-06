1e5c78206b62c81d205a5918fecb80adb3b5543e
André de Souza Alves <alves.andre.ds@gmail.com>
2021-08-05 23:02:12 -0300

    Setting the Product Favorites counter

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,19 +1,21 @@
 class Product < ApplicationRecord
-
-  belongs_to :productable, polymorphic: true
-  validates :name, presence: true, uniqueness: {case_sensitive: false}
-  validates :description, presence: true
-  validates :price, presence: true, numericality: { greater_than: 0}
-  validates :featured, presence: true, if: -> { featured.nil? }
-
-  has_many :product_categories, dependent: :destroy
-  has_many :categories, through: :product_categories
-
-  has_one_attached :image
-  validates :image, presence: true
-  validates :status, presence: true
-  enum status: {available: 1, unavailable: 2}
-
   include LikeSearchable
   include Paginatable
+
+  belongs_to :productable, polymorphic: true
+  has_many :product_categories, dependent: :destroy
+  has_many :categories, through: :product_categories
+  has_many :wish_items
+
+  has_one_attached :image
+
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
+  validates :description, presence: true
+  validates :price, presence: true, numericality: { greater_than: 0 }
+  validates :featured, presence: true, if: -> { featured.nil? }
+  validates :image, presence: true
+  validates :status, presence: true
+
+  enum status: { available: 1, unavailable: 2 }
+
 end
